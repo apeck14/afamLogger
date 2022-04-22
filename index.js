566e999c68c72408a0258ebd4cc7461ef50efe70
Aaron Peck <aaronpeck_14@yahoo.com>
2022-04-21 19:07:12 -0500

    bug fixes + added 7x elixir

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const fs = require("fs");
-const { schedule } = require("node-cron");
-const mongo = require("./src/util/mongo");
-mongo.init();
+const { readdirSync } = require("fs")
+const { schedule } = require("node-cron")
+const mongo = require("./src/util/mongo")
+mongo.init()
 
-const { Client, Collection, Intents } = require("discord.js");
+const { Client, Intents } = require("discord.js")
 
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-    ]
-});
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_TYPING,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+	],
+})
 
-const events = fs.readdirSync("./src/events");
+const events = readdirSync("./src/events")
 
 events.forEach((event) => {
-    const eventFile = require(`./src/events/${event}`);
-    if (eventFile.oneTime) {
-        client.once(eventFile.event, (...args) => eventFile.run(client, mongo.db, ...args));
-    } else {
-        client.on(eventFile.event, (...args) => eventFile.run(client, mongo.db, ...args));
-    }
-});
+	const eventFile = require(`./src/events/${event}`)
+	if (eventFile.oneTime) {
+		client.once(eventFile.event, (...args) => eventFile.run(client, mongo.db, ...args))
+	} else {
+		client.on(eventFile.event, (...args) => eventFile.run(client, mongo.db, ...args))
+	}
+})
 
-const jobs = fs.readdirSync('./src/jobs');
+const jobs = readdirSync("./src/jobs")
 
-for (const job of jobs) { //start all cron jobs
-    try {
-        require.resolve(`./src/jobs/${job}`); //make sure module exists before requiring
-        const jobFile = require(`./src/jobs/${job}`);
-
-        const newJob = schedule(jobFile.expression, () => jobFile.run(client, mongo.db));
-        newJob.start();
-    } catch (e) {
-        console.log(e);
-        continue;
-    }
+for (const job of jobs) {
+	//start all cron jobs
+	try {
+		require.resolve(`./src/jobs/${job}`) //make sure module exists before requiring
+		const jobFile = require(`./src/jobs/${job}`)
+		const newJob = schedule(jobFile.expression, () => jobFile.run(client, mongo.db))
+		newJob.start()
+	} catch (e) {
+		console.log(e)
+		continue
+	}
 }
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
 
-process.on('unhandledRejection', e => {
-    console.log(e);
-    return;
-});
+process.on("unhandledRejection", (e) => {
+	console.log(e)
+	return
+})
--- src/util/functions.js
+++ src/util/functions.js
@@ -1,70 +1,76 @@
-
 module.exports = {
-    formatTag: tag => {
-        if (typeof tag !== 'string') return;
+	formatTag: (tag) => {
+		if (typeof tag !== "string") return
 
-        return `#${tag.toUpperCase().replace(/[^0-9a-z]/gi, '').replace(/O/g, '0').replace(/o/g, '0')}`;
-    },
-    parseDate: date => {
-        if (date instanceof Date) return date;
-        try {
-            return new Date(Date.UTC(
-                date.substr(0, 4),
-                date.substr(4, 2) - 1,
-                date.substr(6, 2),
-                date.substr(9, 2),
-                date.substr(11, 2),
-                date.substr(13, 2),
-            ));
-        } catch (e) {
-            console.log(`Error (parseDate): ${date}`);
-        }
-    },
-    relativeDateStr: date => {
-        const now = new Date();
+		return `#${tag
+			.toUpperCase()
+			.replace(/[^0-9a-z]/gi, "")
+			.replace(/O/g, "0")
+			.replace(/o/g, "0")}`
+	},
+	parseDate: (date) => {
+		if (date instanceof Date) return date
+		try {
+			return new Date(
+				Date.UTC(
+					date.substr(0, 4),
+					date.substr(4, 2) - 1,
+					date.substr(6, 2),
+					date.substr(9, 2),
+					date.substr(11, 2),
+					date.substr(13, 2)
+				)
+			)
+		} catch (e) {
+			console.log(`Error (parseDate): ${date}`)
+		}
+	},
+	relativeDateStr: (date) => {
+		const now = new Date()
 
-        let diffMs = now.getTime() - date.getTime();
+		let diffMs = now.getTime() - date.getTime()
 
-        let str = '';
+		let str = ""
 
-        //check for days
-        const diffDays = parseInt(diffMs / (1000 * 60 * 60 * 24));
-        if (diffDays) {
-            str += `${diffDays}d `;
-            diffMs -= diffDays * (1000 * 60 * 60 * 24);
-        }
+		//check for days
+		const diffDays = parseInt(diffMs / (1000 * 60 * 60 * 24))
+		if (diffDays) {
+			str += `${diffDays}d `
+			diffMs -= diffDays * (1000 * 60 * 60 * 24)
+		}
 
-        //check for hours
-        const diffHours = parseInt(diffMs / (1000 * 60 * 60));
-        if (diffHours) {
-            str += `${diffHours}h `;
-            diffMs -= diffHours * (1000 * 60 * 60);
-        }
+		//check for hours
+		const diffHours = parseInt(diffMs / (1000 * 60 * 60))
+		if (diffHours) {
+			str += `${diffHours}h `
+			diffMs -= diffHours * (1000 * 60 * 60)
+		}
 
-        //check for mins
-        const diffMins = parseInt(diffMs / (1000 * 60));
-        if (diffMins) {
-            str += `${diffMins}m `;
-            diffMs -= diffMins * (1000 * 60);
-        }
+		//check for mins
+		const diffMins = parseInt(diffMs / (1000 * 60))
+		if (diffMins) {
+			str += `${diffMins}m `
+			diffMs -= diffMins * (1000 * 60)
+		}
 
-        //check for mins
-        const diffSecs = parseInt(diffMs / 1000);
-        if (diffSecs) {
-            str += `${diffSecs}s `;
-            diffMs -= diffSecs * 1000;
-        }
+		//check for mins
+		const diffSecs = parseInt(diffMs / 1000)
+		if (diffSecs) {
+			str += `${diffSecs}s `
+			diffMs -= diffSecs * 1000
+		}
 
-        return str.trim() + ' ago';
-    },
-    isWinner: (crowns, oppCrowns, matchCount) => {
-        if (matchCount < 3) return crowns > oppCrowns;
-        else { //matchCount === 3
-            if (crowns < 2) return false;
-            else if (oppCrowns < 2) return true;
-            else if (Math.abs(crowns - oppCrowns) <= 1) return "N/A";
-            else if (crowns > oppCrowns) return true;
-            return false;
-        }
-    }
+		return str.trim() + " ago"
+	},
+	isWinner: (crowns, oppCrowns, matchCount) => {
+		if (matchCount < 3) return crowns > oppCrowns
+		else {
+			//matchCount === 3
+			if (crowns < 2) return false
+			else if (oppCrowns < 2) return true
+			else if (Math.abs(crowns - oppCrowns) <= 1) return "N/A"
+			else if (crowns > oppCrowns) return true
+			return false
+		}
+	},
 }

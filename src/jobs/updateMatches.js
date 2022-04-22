6b7fecbf4ce667414d079dd2805619e71d753381
Aaron Peck <aaronpeck_14@yahoo.com>
2022-04-21 19:07:50 -0500

    changed to 5 mins

--- src/jobs/updateMatches.js
+++ src/jobs/updateMatches.js
@@ -5,7 +5,7 @@ const specialGameModes = require("../static/specialGamemodes")
 const createMatchImg = require("../util/createMatchImgs")
 
 module.exports = {
-	expression: "*/2 * * * *", //every 5 mins
+	expression: "*/5 * * * *", //every 5 mins
 	run: async (client, db) => {
 		console.log("Updating matches...")
 
